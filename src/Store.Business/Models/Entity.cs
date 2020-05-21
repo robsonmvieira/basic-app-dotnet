59e48807368866dec6a0a7bfd51ec68de370cb7d
Henrique Mendonca <henrique.mendonca@vh.com.br>
2020-05-21 19:03:59 -0300

    first commit

---
+++ src/Store.Business/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Business.Models
+{
+    public abstract class Entity
+    {
+
+    }
+}
---
+++ src/Store.IO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Store.IO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
