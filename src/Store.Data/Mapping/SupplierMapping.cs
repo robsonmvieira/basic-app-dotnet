0cbdaae540c2ea667eba29cde2319e097b462d39
Henrique Mendonca <henrique.mendonca@vh.com.br>
2020-05-22 10:40:03 -0300

    feat: add Supplier Fluent Mapping

---
+++ src/Store.Data/Mapping/SupplierMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Data.Mapping
+{
+    class SupplierMapping : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Doc).IsRequired().HasColumnType("varchar(20)");
+
+
+            builder.HasOne(s => s.Address)
+                .WithOne(a => a.Supplier);
+
+            builder.HasMany(s => s.Products)
+                .WithOne(p => p.Supplier)
+                .HasForeignKey(p => p.SupplierId);
+
+            builder.ToTable("suppliers");
+        }
+    }
+}
